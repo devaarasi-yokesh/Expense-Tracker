c807e9a00997e64ead47ff62c39b2537e1ca904e
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2024-12-27 23:18:21 +0530

    Update App.jsx

--- Main/App.jsx
+++ Main/App.jsx
@@ -2,14 +2,17 @@ import { useState,useRef } from 'react'
 import './App.css'
 import ViewTracker from './ListItems';
 import Input from './addItem';
-
+import Header from './Header';
+import Footer from './Footer';
 
 
 function App() {
 const [add, setAdd] = useState(true);
 const [list, setList] = useState(false);
-const [inputVals, setInputVals] = useState([]);
-
+const [inputVals, setInputVals] = useState('');
+const [incomeVal, setIncomeVal] = useState('0.00');
+const [expenseVal, setExpenseVal] = useState('0.00')
+const [savingVal, setSavingVal] = useState("0.00")
 
 function setTransit(val1,val2){
   setAdd(val1);
@@ -18,9 +21,11 @@ function setTransit(val1,val2){
   return (
     <>
       <div>
-      {add && <Input onClick={setTransit} share={setInputVals}/>}
-      {list && <ViewTracker  onClick={setTransit} show={inputVals}/>}
+      <Header showIncome={incomeVal} showExpense={expenseVal} showSaving={savingVal}/>
+      {add && <Input onClick={setTransit} share={setInputVals} prevInc={incomeVal} income={setIncomeVal} prevSav={savingVal} saving={setSavingVal} prevExp={expenseVal} expense={setExpenseVal}/>}
+      {list && <ViewTracker   input={inputVals}/>}
       </div>
+      <Footer trans={setTransit}/>
     </>
   )
 }
