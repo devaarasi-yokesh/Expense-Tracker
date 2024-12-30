ac3e79d6d6c7dfb31ce346d09b2cfc9488e43bc5
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2024-12-31 00:01:35 +0530

    Update App.jsx

--- Main/App.jsx
+++ Main/App.jsx
@@ -9,21 +9,24 @@ import Footer from './Footer';
 function App() {
 const [add, setAdd] = useState(true);
 const [list, setList] = useState(false);
-const [inputVals, setInputVals] = useState('');
+const [inputVals, setInputVals] = useState([]);
 const [incomeVal, setIncomeVal] = useState('0.00');
 const [expenseVal, setExpenseVal] = useState('0.00')
 const [savingVal, setSavingVal] = useState("0.00")
+const [items,setItems] = useState({});
+const [month, setMonth] = useState('');
 
 function setTransit(val1,val2){
   setAdd(val1);
   setList(val2);
 }
+console.log(items)
   return (
     <>
       <div>
       <Header showIncome={incomeVal} showExpense={expenseVal} showSaving={savingVal}/>
-      {add && <Input onClick={setTransit} share={setInputVals} prevInc={incomeVal} income={setIncomeVal} prevSav={savingVal} saving={setSavingVal} prevExp={expenseVal} expense={setExpenseVal}/>}
-      {list && <ViewTracker   input={inputVals}/>}
+      {add && <Input listData={setItems} changeMonth={setMonth} onClick={setTransit} share={setInputVals} prevInc={incomeVal} income={setIncomeVal} prevSav={savingVal} saving={setSavingVal} prevExp={expenseVal} expense={setExpenseVal}/>}
+      {list && <ViewTracker   input={inputVals} listShow={items} shareMonth={month}/>}
       </div>
       <Footer trans={setTransit}/>
     </>
