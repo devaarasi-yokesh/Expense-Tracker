1907a6f1c8491e68c7da00381325594bee1b7de9
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2025-01-16 23:19:13 +0530

    Update ListItems.jsx

--- Main/ListItems.jsx
+++ Main/ListItems.jsx
@@ -2,60 +2,46 @@ import { useState } from 'react'
 import './ListItems.css'
 
 function ViewTracker(props){
-   const [val, setVal] = useState(" ");
-   const [title, setTitle] = useState("");
-   
-   const styling = {display:"flex",justifyItems:"center",flexDirection:"row",justifyContent:"space-evenly"};
-   console.log("listitems", props.listShow)
-   const firstItem = {4: {title: 'Groceries', amt: 45, mode: 'cash'}};
+
+    const styling = {display:"flex",justifyItems:"center",flexDirection:"row",justifyContent:"space-evenly"};
+    console.log("listitems", props.listShow, props.shareFilteredMonthItems) // Testing point
+function handleChange(){
+    props.listShow.map((data) => data.section = false);
+}
+
     return(
         <>
-        <div className='monthlyHeading' style={styling}>
-        <h1>{props.shareTitle}</h1> {/* ListShow - Object holding all input fields*/}
         
-        </div>
-        <div className='monthlyContent' style={styling}>
-        <table>
-            <thead>
-            <tr>
-                <th>Title</th>
-                <th>Amount</th>
-                <th>Mode</th>
-            </tr>
-            </thead>
-            <tbody>
-            
-           {Object.keys(props.listShow).map((data,index) => {
-                console.log(data,"data",props.shareTitle)
-                if(data !== 'August2025'){
-                    console.log("yesmatched", props.shareTitle)
-                    return Object.values(props.listShow[data]).map((value,index) => {
-                    console.log("insidemap",value)
-
-                        // if(value.hasOwnProperty(props.shareDate)){
-                        console.log("shouldnotbe", value.title)
-                            return(
-                                <tr>
-                                <td>{value.title}</td>
-                                <td>{value.amt}</td>
-                                <td>{value.mode}</td>
-                                </tr>
-                            );
-                        // }
-                    });
-                }
-            })}
-           
-            <tr>
-                <td></td>
-            </tr>
-            </tbody>
-        </table>
-        
-        </div>
-        
+               
+                    <div className='monthlyContent' style={styling}>
+                    
+                        {props.listShow.map((data2) => {
+                           console.log( data2.month_year,typeof(data2.month_year))
+                           if(data2.section){
+                               return(
+                                <table>
+                                <thead>
+                                    <tr><td>{data2.month_year}</td></tr>
+                                </thead>
+                                 <tbody>
+                                   <tr>
+                                       <td onChange={handleChange}>{data2.title}</td>
+                                       <td>{data2.amt}</td>
+                                       <td>{data2.mode}</td>
+                                   </tr>
+                                   <tr>
+                            <td></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                               )
+                            }
+                           
+                        })}
+                        
+                    </div>
         </>
-    )
+)
 }
 
 export default ViewTracker;
