a59f824f645c1265dec7335561bf96e90c3c0c51
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2025-01-16 23:20:03 +0530

    Update App.jsx

--- Main/App.jsx
+++ Main/App.jsx
@@ -7,36 +7,36 @@ import Footer from './Footer';
 
 
 function App() {
-const [add, setAdd] = useState(true); // Flag to set the home screen 
-const [list, setList] = useState(false); // Flag to set the listItems screen
-const [inputVals, setInputVals] = useState([]); // Array having title and amount input values
+const [add, setAdd] = useState(true);               // Flag to set the home screen 
+const [list, setList] = useState(false);            // Flag to set the listItems screen
+const [inputVals, setInputVals] = useState([]);     // Array having title and amount input values
 const [incomeVal, setIncomeVal] = useState('0.00'); // Header income value initialized
 const [expenseVal, setExpenseVal] = useState('0.00') // Header expense initialized
-const [savingVal, setSavingVal] = useState("0.00") //Header Savings initialized 
-const [items,setItems] = useState({August2025:{4:{ title:"Groceries",
-  amt: 45,
-  mode: "cash",}}}); // object for holding the set of inputs entered by the user
-const [title, setTitle] = useState('August2025');  // Month and year title - ListItems
-const [date, setDate] = useState('');    // Specific date - ListItems
+const [savingVal, setSavingVal] = useState("0.00")  //Header Savings initialized 
+const [items,setItems] = useState([]);              // object for holding the set of inputs entered by the user
+const[filteredMonthItems, setFilteredMonthItems] = useState([]); 
+const [date, setDate] = useState('');               // Specific date - ListItems
+
 
 // To change between Home / ListItems / Chart tabs
 function setTransit(val1,val2){
   setAdd(val1);
   setList(val2);
 }
-console.log(items)
+
 
   return (
     <>
       <div>
 
-      <Header showIncome={incomeVal} showExpense={expenseVal} showSaving={savingVal}/>      {/* Header Label */}
+      <Header showIncome={incomeVal} showExpense={expenseVal} showSaving={savingVal}/>         {/* Header Label */}
 
-      {add && <Input listData={setItems} listObject={items} changeTitle={setTitle} changeDate={setDate} onClick={setTransit}        /* adding Items  */
+      {add && <Input listData={setItems} listObject={items}  changeDate={setDate} onClick={setTransit}   /* adding Items  */
+      shareFilteredMonthItems={filteredMonthItems} changeFilteredMonthItems= {setFilteredMonthItems}
       share={setInputVals} prevInc={incomeVal} income={setIncomeVal} prevSav={savingVal} 
       saving={setSavingVal} prevExp={expenseVal} expense={setExpenseVal}/>}
 
-      {list && <ViewTracker   input={inputVals} listShow={items} shareTitle={title} shareDate={date}/>}       {/*/* Listing Items */}
+      {list && <ViewTracker   input={inputVals} listShow={items} changeList={setItems} shareDate={date} shareFilteredMonthItems={filteredMonthItems}/>}           {/*/* Listing Items */}
 
       </div>
 
